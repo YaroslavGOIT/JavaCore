9b8350fcf101c4a2d4cdd70f84d685f0948350d5
YaroslavGOIT <yaroslav.http@gmail.com>
2016-08-20 14:17:06 +0300

    initial commit

---
+++ src/FourthClass.java
@@ -0,0 +1,11 @@
+/**
+ * Created by admin-pc on 20.08.2016.
+ */
+public class FourthClass {
+    public static void main(String[] args){
+
+        System.out.print(" My first programs on java... ");
+
+    }
+}
+
