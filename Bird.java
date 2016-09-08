d610373ac81a510c4188d95f158786a70f47ae65
YaroslavGOIT <yaroslav.http@gmail.com>
2016-09-08 17:33:43 +0300

    Update Bird.java

--- Bird.java
+++ Bird.java
@@ -3,12 +3,17 @@ package Module_3;
 /**
  * Created by admin-pc on 04.09.2016.
  */
-public class Bird {
-    void fly(){
+public class Bird extends Animal {
+    int fly(){
         System.out.println("I am flying");
+        return 0;
     }
 
     void sing(){
         System.out.println("I am singing");
+    }
+
+    void bird(){
+        System.out.println("I am Bird");
     }
 }
