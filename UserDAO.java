ae9cc910f4a99b3812c4a741dada77b6591b79f8
YaroslavGOIT <yaroslav.http@gmail.com>
2016-10-29 20:53:51 +0300

    Add files via upload

---
+++ UserDAO.java
@@ -0,0 +1,11 @@
+package Module_8;
+
+import java.util.Map;
+
+public class UserDAO<T,K> extends AbstractDAOImpl {
+    private T id;
+    private K name;
+    public void findName(T id, Map<T, K> o){
+        System.out.println(o.get(id));
+    }
+}
